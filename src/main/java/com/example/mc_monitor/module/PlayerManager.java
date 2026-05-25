cf1783d49c28361d07003af4cd6c6aa47270c20f
WonJeongYeon <sionok.dev@gmail.com>
2026-05-25 22:15:55 +0900

    feat: module에 postConstruct 코드만 추가

--- src/main/java/com/example/mc_monitor/module/PlayerManager.java
+++ src/main/java/com/example/mc_monitor/module/PlayerManager.java
@@ -1,4 +1,16 @@
 package com.example.mc_monitor.module;
 
+import jakarta.annotation.PostConstruct;
+
 public class PlayerManager {
+
+    @PostConstruct
+    public void init() {
+        run();
+    }
+
+    private void run() {
+        // 여기다 일반코드건 Webflux코드건 자유로이 작성
+    }
+
 }
